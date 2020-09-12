c887ee2b659b1d5cd7f62394b14bd6d9d4dcf421
shmakova <shmakova-nastya@yandex.ru>
2020-09-12 16:31:40 +0300

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Anastasia Shmakova on 12.09.2020.
+//  Copyright © 2020 Shmakova Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
